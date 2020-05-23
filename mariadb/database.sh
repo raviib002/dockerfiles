73aa61598df1b0f69632a1cb86abb26bb8e2d22e
baryonsit <baryonsit@gmail.com>
2020-05-23 07:50:27 +0000

    updated

--- mariadb/database.sh
+++ mariadb/database.sh
@@ -5,15 +5,15 @@ DATE=$(date +%d-%m-%Y)
 #USER="root"
 #PASS="mysql"
 #mysqldump $HOST $PORT -u$USER -p$PASS test11 > /home/test11_$DATE.sql
-HPUP="--host=127.0.0.1 --port=3306 -uroot -pmysql"
+HPUP="--host=172.20.128.2 --port=3306 -uroot -pmysql"
 #HPUP="-uroot -pmysql"
 mysqladmin $HPUP --force create wordprestest
 #mysqladmin $HPUP create wordprestest
 #mysqladmin $HPUP --force drop wordprestest
-mysql $HPUP -e "create user 'wordprestest'@'localhost' IDENTIFIED BY 'wordprestest';"
+mysql $HPUP -e "create user 'wordprestest'@'%' IDENTIFIED BY 'wordprestest';"
 #mysql $HPUP -e "drop user 'wordprestest'@'%';"
-mysql $HPUP -e "grant all privileges on wordprestest .* to 'wordprestest'@'localhost';"
+mysql $HPUP -e "grant all privileges on wordprestest .* to 'wordprestest'@'%';"
 #mysqldump $HPUP wordprestest > /home/wordprestest/backups/wordprestest_$DATE.sql
-mysql $HPUP -e "source /home/wordprestest/backups/wordprestest_07_may_2020.sql" wordprestest
+mysql $HPUP -e "source /home/wordprestest/backups/wordtest_may_18_2020.sql" wordprestest
 
 
